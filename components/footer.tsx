2011a6f465cc4a261494c52639f54f8367dac007
Silverhulk <hulksilver1@gmail.com>
2026-05-06 12:37:16 +0000

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
           {/* Brand */}
           <div className="md:col-span-2">
             <Link href="/" className="flex items-center gap-3">
-              <div className="flex size-10 items-center justify-center rounded-lg bg-primary">
+              <div className="flex size-20 items-center justify-center rounded-lg bg-primary">
                 <Image 
                   src="/file_00000000c518724687a64e9f7e791067.png" 
                   alt="SUPREMYX Logo" 
