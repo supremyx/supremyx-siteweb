b987f3b89104ecbdd3bc8cffe585db02851d3be9
Silverhulk <hulksilver1@gmail.com>
2026-05-06 12:33:20 +0000

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -17,8 +17,8 @@ export function Header() {
             <Image 
               src="/file_00000000c518724687a64e9f7e791067.png" 
               alt="SUPREMYX CI Logo" 
-              width={60} 
-              height={60}
+              width={50} 
+              height={50}
               className="size-6"
             />
           </div>
