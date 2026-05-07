170b4714be6d68dd8ee93b3cf4c7e65950a30ed1
Silverhulk <hulksilver1@gmail.com>
2026-05-07 06:41:42 +0000

    Update registration-dialog.tsx

--- components/registration-dialog.tsx
+++ components/registration-dialog.tsx
@@ -122,7 +122,7 @@ export function RegistrationDialog({ open, onOpenChange }: RegistrationDialogPro
 
             <Field>
               <FieldLabel htmlFor="phonenumber">
-                <Mobile className="mr-2 inline size-4" />
+                <User className="mr-2 inline size-4" />
                 Numéro Mobile Money
               </FieldLabel>
               <Input
