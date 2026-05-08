233c28d082b07c305c4435430bdc73ba3c6e33fa
Silverhulk <hulksilver1@gmail.com>
2026-05-08 03:34:17 +0000

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -32,7 +32,7 @@ export function Header() {
             href="/tournois"
             className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground"
           >
-            
+            T
           </Link>
           <Link
             href="/equipes"
