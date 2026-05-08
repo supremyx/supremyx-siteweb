681a8e84d82f80b7fa79f599dc7e711188e0b55a
Silverhulk <hulksilver1@gmail.com>
2026-05-08 04:03:02 +0000

    Update tournaments-section.tsx

--- components/tournaments-section.tsx
+++ components/tournaments-section.tsx
@@ -79,7 +79,7 @@ const tournaments: Tournament[] = [
     date: "",
     time: "",
     prize: "",
-    slots: Illimité,
+    slots: 100,
     registered: 0,
     status: "upcoming",
     type: "squad",
